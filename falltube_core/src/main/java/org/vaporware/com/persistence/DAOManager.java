29835c28f9d0b38cafd1f4f9754ad65290404df0
angelloro <Angel.Loro@alu.uclm.es>
2018-10-18 11:43:38 +0200

    version 0.3.6

--- falltube_core/src/main/java/org/vaporware/com/persistence/DAOManager.java
+++ falltube_core/src/main/java/org/vaporware/com/persistence/DAOManager.java
@@ -17,8 +17,8 @@ import org.vaporware.com.domain.YvdSimplified;
  */
 public class DAOManager {
     
-    private Connection conn;
-    private PreparedStatement ps;
+    private static Connection conn;
+    private static PreparedStatement ps;
     
     public static boolean isVideoOnDatabase(String videoId) {
         //IMPLEMENTAR ESTE METODO
@@ -27,9 +27,48 @@ public class DAOManager {
         return a;
     }
 
-    public static void videoToDatabase(YvdSimplified video) {
-        //IMPLEMENTAR ESTE METODO
-        //SQLManager.getInstance().insert("");
+    public static  void videoToDatabase(YvdSimplified video) {
+        
+		try {
+						
+			String sql = "insert into proyectos values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";			
+			ps = conn.prepareStatement(sql);
+			ps.setString(1, video.getId());
+			ps.setString(2, video.getEtag());
+			ps.setString(3, video.getPublishedAt());
+			ps.setString(4, video.getChannelId());
+			ps.setString(5, video.getTitle());
+                        ps.setString(6, video.getDescription());
+			ps.setString(7, video.getChannelTitle());
+			ps.setString(8, video.getCategoryId());
+			ps.setString(9, video.getDefaultAudioLanguage());
+			ps.setString(10, video.getDuration());
+                        ps.setString(11, video.getDimension());
+			ps.setString(12, video.getDefinition());
+			ps.setBoolean(13, video.isCaption());
+			ps.setBoolean(14, video.isLicensedContent());
+			ps.setString(15, video.getProjection());
+                        ps.setLong(16, video.getViewCount());
+			ps.setLong(17, video.getLikeCount());
+			ps.setLong(18, video.getDislikeCount());
+			ps.setLong(19, video.getCommentCount());
+			ps.setString(20, video.getTags().get(0));
+                        ps.setString(21, video.getTags().get(1));
+			ps.setString(22, video.getTags().get(2));
+			ps.setString(23, video.getTags().get(3));
+			ps.setString(24, video.getTags().get(4));
+			ps.setString(25, video.getTags().get(5));
+			ps.setString(26, video.getTags().get(6));
+			ps.setString(27, video.getTags().get(7));
+			ps.setString(28, video.getTags().get(8));
+			ps.setString(29, video.getTags().get(9));
+                        
+                        SQLManager.getInstance().insert(sql);
+
+			
+		} catch (Exception e) {
+			System.out.println("Error al insertar proyecto en la base de datos.");
+		}
     }
 
     public static void commentsToDatabase(ArrayList<Comment> comments) {
