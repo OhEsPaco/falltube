c951b12e4143e88c2ef7c03fe4bde8866c804a40
OhEsPaco <fcomanuel.garcia4@alu.uclm.es>
2018-11-23 20:20:58 +0100

    Update script.sql

--- script.sql
+++ script.sql
@@ -1,19 +1,19 @@
 CREATE TABLE `video` (
-  `videoId` varchar(45) CHARACTER SET utf8 NOT NULL,
-  `etag` varchar(100) CHARACTER SET utf8 DEFAULT NULL,
-  `publishedAt` varchar(100) CHARACTER SET utf8 DEFAULT NULL,
-  `channelId` varchar(100) CHARACTER SET utf8 DEFAULT NULL,
+  `videoId` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `etag` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `publishedAt` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `channelId` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
   `title` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
   `description` varchar(2000) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
   `channelTitle` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
-  `categoryId` varchar(45) CHARACTER SET utf8 DEFAULT NULL,
-  `defaultAudioLanguage` varchar(45) CHARACTER SET utf8 DEFAULT NULL,
-  `duration` varchar(45) CHARACTER SET utf8 DEFAULT NULL,
-  `dimension` varchar(45) CHARACTER SET utf8 DEFAULT NULL,
-  `definition` varchar(100) CHARACTER SET utf8 DEFAULT NULL,
+  `categoryId` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `defaultAudioLanguage` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `duration` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `dimension` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `definition` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
   `caption` tinyint(4) DEFAULT NULL,
   `licensedContent` tinyint(4) DEFAULT NULL,
-  `projection` varchar(45) CHARACTER SET utf8 DEFAULT NULL,
+  `projection` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
   `viewCount` bigint(15) DEFAULT NULL,
   `likeCount` bigint(15) DEFAULT NULL,
   `dislikeCount` bigint(15) DEFAULT NULL,
@@ -32,12 +32,14 @@ CREATE TABLE `video` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 CREATE TABLE `comments` (
-  `commentId` varchar(45) CHARACTER SET utf8 NOT NULL,
-  `videoId` varchar(45) CHARACTER SET utf8 DEFAULT NULL,
+  `commentId` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `videoId` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
   `authorName` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
-  `authorUrl` varchar(100) CHARACTER SET utf8 DEFAULT NULL,
+  `authorUrl` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
   `comment` varchar(2000) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
   PRIMARY KEY (`commentId`),
   KEY `fkVideoId_idx` (`videoId`),
   CONSTRAINT `fkVideoId` FOREIGN KEY (`videoId`) REFERENCES `video` (`videoid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+SET GLOBAL max_connections = 500;
